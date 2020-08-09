accad993f777d46ca67e80e936b14a61d9442ceb
Bishal Bista <b3hal9@gmail.com>
2020-08-09 13:08:49 +0545

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
-const port = process.env || 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.render("index");
