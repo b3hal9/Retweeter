c1b583ad185f5d48d336c63c98e97c56f2e9cbcd
Bishal Bista <b3hal9@gmail.com>
2020-08-09 12:43:13 +0545

    v1.0

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const bot = require("./utils/BotFunction");
-
+require("dotenv").config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
-const port = 5000;
+const port = process.env || 5000;
 
 app.get("/", (req, res) => {
   res.render("index");
